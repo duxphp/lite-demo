84d537625fd1935f54d988558aa9fbedba4372fc
mr.l <admin@duxphp.com>
2023-01-29 16:52:07 +0800

    update

---
+++ src/Test/App.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Test;
+
+/**
+ * Application Registration
+ */
+class App extends \Dux\App\AppExtend
+{
+	public string $name = 'App Name';
+	public string $description = 'App Desc';
+}
---
+++ src/Test/Config/Permission.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Test\Config;
+
+use Dux\Permission\Permission as DuxPermission;
+
+class Permission
+{
+}
